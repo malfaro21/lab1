8f24cd76467f607c89f609ebfe1385e18d2b0cbd
malfaro21 <malfaro21@students.desu.edu>
2023-10-07 14:47:19 +0000

    mystery explode

--- lab1.c
+++ lab1.c
@@ -1,5 +1,16 @@
-/*
- * Implement your solution in thi file
- *
- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+char* readString(char* fileName)
+{
+    int main();
+}
+
+char* mysteryExplode(const char* str)
+{
+    int length = strlen(str);
+    if (length == 0) return NULL;
+    int newSize = (length * (length +1)) /2;
+    
+}
