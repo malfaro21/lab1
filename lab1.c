d4398e25e90c5417c17ecaf963f20bb259e5e034
malfaro21 <malfaro21@students.desu.edu>
2023-10-08 16:25:25 +0000

    mystery for loop

--- lab1.c
+++ lab1.c
@@ -15,4 +15,9 @@ char* mysteryExplode(const char* str)
     int newSize = (length * (length +1)) /2;
     char* exploded = (char*)malloc(newSize + 1);
     
+    if(!exploded) return NULL;
+    int j = 0;
+    for(int i=0; i<length; i++){
+        
+    }
 }
