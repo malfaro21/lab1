dfdf3ed27e670f79642a4252995bc992cab4cb60
malfaro21 <malfaro21@students.desu.edu>
2023-10-08 21:35:38 +0000

    mystery exploded

--- lab1.c
+++ lab1.c
@@ -23,19 +23,10 @@ char* readString(char* fileName)
 
 char* mysteryExplode(const char* str)
 {
-    int length = strlen(str);
-    if (length == 0) return NULL;
-
-    int newSize = (length * (length +1)) /2;
-    char* exploded = (char*)malloc(newSize + 1);
-    
-    if(!exploded) return NULL;
-    int j = 0;
-    for(int i = 0; i<length; i++){
-        for(int k = 0; k <= i; k++){
-            exploded[j++] = str[k];
-        }
+    int len = (strlen(str) * (strlen(str) + 1) /2) + 1;
+    char* ans = (char*) malloc(len);
+    if(!ans){
+        return NULL;
     }
-    exploded[j] = '/0';
-    return exploded;
+    
 }
